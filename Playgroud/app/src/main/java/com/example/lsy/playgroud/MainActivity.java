6bc7f4206ffaccc2e6fde4980f050df02ad6726a
Sanying Li <sanyingli@gwu.edu>
2016-09-19 01:50:13 -0400

    support different devices

--- Playgroud/app/src/main/java/com/example/lsy/playgroud/MainActivity.java
+++ Playgroud/app/src/main/java/com/example/lsy/playgroud/MainActivity.java
@@ -24,5 +24,7 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE,message);
         startActivity(intent);
+
+
     }
 }
