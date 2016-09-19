97540410ac4353011fa991dab00a65c67a23d870
Sanying Li <sanyingli@gwu.edu>
2016-09-19 18:10:18 -0400

    no message

--- Playgroud/app/src/main/java/com/example/lsy/playgroud/MainActivity.java
+++ Playgroud/app/src/main/java/com/example/lsy/playgroud/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.lsy.playgroud;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    //TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //set the user interface layout for this Activity
+        //The layout file is defined in the project res/layout/activity_main.xml file
         setContentView(R.layout.activity_main);
+
+        // Initialize member TextView so we can manipulate it later
+        //mTextView =(TextView) findViewById(R.id.edit_message);
+
+        //make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+        {
+            //for the main activity, make sure the app icon in the action bar
+            //does not behave as button
+            //ActionBar actionBar = getActionBar();
+            //actionBar.setHomeButtonEnabled(false);
+        }
     }
 
     //Called when click the Send button
